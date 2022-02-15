77ead1c6b89f8e538f3c414bfb01ebfd5ac91269
Devin Riley <devinriley@gmail.com>
2022-02-14 19:53:34 -0600

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use ethers::prelude::*;
-fn main() {
-    Abigen::new("UniV2Router", "./uniswap-v2-abi.json")
-        .unwrap()
-        .generate()
-        .unwrap()
-        .write_to_file("uni-v2-router.rs")
-        .unwrap();
+// use ethers::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    // Abigen::new("UniV2Router", "./uniswap-v2-abi.json")
+    //     .unwrap()
+    //     .generate()
+    //     .unwrap()
+    //     .write_to_file("uni-v2-router.rs")
+    //     .unwrap();
+
+    let url = format!(
+        "https://api.{}.io/api\
+        ?module=contract\
+        &action=getabi\
+        &address={}\
+        &apikey={}",
+    );
+    let resp = reqwest::get(url).await;
 }
